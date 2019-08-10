669164821b4443213ff3ded16119104289570491
psjong <psj7007@naver.com>
2019-08-10 16:52:24 +0900

    20190810 16:52

--- Github/Workspace/WhatNameAge.java
+++ Github/Workspace/WhatNameAge.java
@@ -1,11 +1,11 @@
 import java.util.Scanner ;
 class WhatNameAge {
     public static void main (String[] agrs) {
+        Scanner scanner = new Scanner (System.in) ;
         System.out.println("안녕~");
-        System.out.println("내 이름은 피세종이야");
+        System.out.println("내 이름은 홍길동이야");
         System.out.println("너는 이름이 뭐니?");
         String name = "";
-        Scanner scanner = new Scanner (System.in);
         name = scanner.next();
         System.out.println("내 이름은 " + name + "이야");
         System.out.println("반갑다~  " + name + "아");
