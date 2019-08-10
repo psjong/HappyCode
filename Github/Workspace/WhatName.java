2759523402ce5f081fc0762d0ff47e7100ac719c
psjong <psj7007@naver.com>
2019-08-10 16:01:57 +0900

    20190810 16:01

--- Github/Workspace/WhatName.java
+++ Github/Workspace/WhatName.java
@@ -1,5 +1,14 @@
+import java.util.Scanner ;
 class WhatName {
     public static void main (String[] args){
         System.out.println("이름이 뭐니?");
+        String name = "";
+        Scanner scanner = new Scanner(System.in);
+        name = scanner.next();
+        System.out.println("내 이름은 " + name + "이야~");
+        System.out.println("몇 살이니?");
+        int age = 0 ;
+        age = scanner.nextInt();
+        System.out.print("내 나이는 " + age + "살 이야~" );
     }
 }
